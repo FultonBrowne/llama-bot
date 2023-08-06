0d648e93d73167e79cd6a6e3c07474e0e2d889b7
Fulton Browne <fultonbrowne@pm.me>
2023-08-05 22:49:24 -0500

    It works mostly

--- bot/bot.go
+++ bot/bot.go
@@ -4,6 +4,7 @@ import (
 	"DiscordBot/config"
 	"DiscordBot/utils"
 	"fmt"
+	"context"
 	"github.com/bwmarrin/discordgo"
 	"os"
 	"os/signal"
@@ -11,10 +12,11 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+	"github.com/jmorganca/ollama/api"
 )
 var botID string
 var client *discordgo.Session
-
+var apiClient  *api.Client
 func Start() {
 	session, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
@@ -23,7 +25,7 @@ func Start() {
 	}
 	session.AddHandler(message)
 	session.AddHandler(ready)
-
+	apiClient = api.NewClient()
 	fmt.Print("Bot is online")
 	defer session.Close()
 	if err = session.Open(); err != nil {
@@ -38,13 +40,26 @@ func Start() {
 
 func ready(bot *discordgo.Session, event *discordgo.Ready) {
 	guildsSize := len(bot.State.Guilds)
-	bot.UpdateGameStatus(0, strconv.Itoa(guildsSize) + " guilds!")
+	bot.UpdateGameStatus(0, strconv.Itoa(guildsSize) + " with your emotions")
+}
+
+func IsDM(s *discordgo.Session, m *discordgo.MessageCreate) bool {
+	channel, err := s.Channel(m.ChannelID)
+	if err != nil {
+		return false
+	}
+	if channel.Type != discordgo.ChannelTypeDM {
+		return false
+	}
+	return true
 }
 
 func message(bot *discordgo.Session, message *discordgo.MessageCreate) {
+	/* Discord bot commands */
 	if message.Author.Bot { return }
-	switch {
-	case strings.HasPrefix(message.Content, config.BotPrefix):
+	
+	if strings.HasPrefix(message.Content, "&") || IsDM(bot, message){
+		fmt.Println("content:", message.Content)
 		ping := bot.HeartbeatLatency().Truncate(60)
 		if message.Content == "&ping" {
 			bot.ChannelMessageSend(message.ChannelID,`My latency is **` + ping.String() + `**!`)
@@ -70,7 +85,25 @@ func message(bot *discordgo.Session, message *discordgo.MessageCreate) {
 				AddField("Total guilds:", strconv.Itoa(guilds)).MessageEmbed
 			bot.ChannelMessageSendEmbed(message.ChannelID, embed)
 		} else {
-			
+			var latest api.GenerateResponse
+			var output string
+			generateContext := []int{} // TODO: Get conversation context
+			request := api.GenerateRequest{Model: "llama2", Prompt: message.Content, Context: generateContext}
+			fn := func(response api.GenerateResponse) error {
+
+				latest = response
+				output = output + response.Response
+
+				fmt.Print(response.Response)
+				return nil
+			}
+			if err := apiClient.Generate(context.Background(), &request, fn); err != nil {
+				bot.ChannelMessageSend(message.ChannelID, "There seems to be a problem, please cry to the developer (FultonBrowne on github)")
+				return
+			}
+			bot.ChannelMessageSend(message.ChannelID, output)
+			latest.Summary()
 		}
 	}
+	
 }
